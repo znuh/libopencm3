38ef8ba96fbcb8eb1cfce736132e6076de1b017b
znuh <Zn000h@gmail.com>
2025-02-21 20:42:04 +0100

    add doc

---
+++ include/libopencm3/stm32/c0/doc-stm32c0.h
@@ -0,0 +1,36 @@
+/** @page libopencm3 STM32C0
+
+@version 1.0.0
+
+@date 21 January 2025
+
+API documentation for ST Microelectronics STM32C0 Cortex M0+ series
+
+LGPL License Terms @ref lgpl_license
+*/
+
+/** @defgroup peripheral_apis Peripheral APIs
+ * APIs for device peripherals
+ */
+
+/** @defgroup STM32C0xx STM32C0xx
+Libraries for ST Microelectronics STM32C0xx series.
+
+@version 1.0.0
+
+@date 21 January 2025
+
+LGPL License Terms @ref lgpl_license
+*/
+
+/** @defgroup STM32C0xx_defines STM32C0xx Defines
+
+@brief Defined Constants and Types for the STM32C0xx series
+
+@version 1.0.0
+
+@date 21 January 2025
+
+LGPL License Terms @ref lgpl_license
+*/
+
